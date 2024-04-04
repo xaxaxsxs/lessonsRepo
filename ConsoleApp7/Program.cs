2fe9b76697ba28730040eb52b3096cdd5b793fcc
xaxaxsxs <xomiachok.ziuzi2016@yandex.ru>
2024-04-04 14:04:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class ConsoleApp7
+{
+    static void Main()
+    {
+        double n1, n2;
+        n1 = 10;
+        n2 = 20;
+
+        double answ = 0;
+
+        answ = n1 - n2;
+
+        Console.WriteLine(answ + " Разность ");
+
+        answ = n1 + n2;
+
+        Console.WriteLine(answ + " Сумма");
+
+        answ = n1 * n2;
+
+        Console.WriteLine(answ + " Произведение");
+
+        answ = n1 / n2;
+
+        Console.WriteLine(answ + " Частное");
+
+        Console.WriteLine("числа: " + n1 + "  и  " + n2);
+    }
+}
